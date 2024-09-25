80aae536b0924691421f3583218b49d7ad537197
ngochan21098 <ngochan21098@gmail.com>
2024-09-25 21:17:10 +0700

    Bai tap function 2

---
+++ Bai-tap-Function-2/Bai3.py
@@ -0,0 +1,17 @@
+def print_rec(m, n):
+  """
+  In ra hình chữ nhật có chiều dài m và chiều rộng n.
+
+  Args:
+    m: Chiều dài của hình chữ nhật.
+    n: Chiều rộng của hình chữ nhật.
+  """
+  if m <= n:
+        print('Fail')
+  else:   
+    for i in range(n):
+        for j in range(m):
+         print('*', end='')
+        print() 
+
+print_rec(2, 4)
